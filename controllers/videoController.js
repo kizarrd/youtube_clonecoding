d932c8c1192098e7ee7b4da100f24c58dc85707f
kizarrd <51534324+kizarrd@users.noreply.github.com>
2021-01-13 15:53:49 +0900

    mongo stuffs eslint webpack scss #5.3

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -11,11 +11,19 @@ export const home = async (req, res) => {
     }
 }
     
-export const search = (req, res) => {
+export const search = async (req, res) => {
     const {
         query: {term: searchingBy}
     } = req 
     // this line equals const searchingBy = request.query.term;
+
+    let videos = [];
+
+    try{
+        videos = await Video.find({title: { $regex: searchingBy, $options: "i" }});
+    }catch(error){
+        console.log(error);
+    }
     res.render("search", { pageTitle: "Search", searchingBy: searchingBy, videos });
 }
 export const getUpload = (req,res) => 
@@ -77,6 +85,8 @@ export const deleteVideo = async (req,res) => {
     }=req;
     try{
         await Video.findOneAndRemove({_id: id});
-    } catch(error) { }
+    } catch(error) { 
+        console.log(error);
+    }
     res.redirect(routes.home);
 }
