a778a4aa506c3e85dd40d03efafe5c0346485bf7
kizarrd <51534324+kizarrd@users.noreply.github.com>
2021-01-13 20:35:01 +0900

    passportjs cookies createUser(join) session MongoStore(connect-mongo) controling routes with middlewares #6.5

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import express from "express";
+import passport from "passport";
+import mongoose from "mongoose";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 import globalRouter from "./routers/globalRouter";
 import helmet from "helmet";
 import { localsMiddleware } from "./middlewares";
@@ -9,7 +13,11 @@ import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
+import "./passport";
+
 const app = express();
+
+const CookieStore = MongoStore(session)
 
 app.use(helmet());
 app.use(function(req, res, next) {
@@ -23,6 +31,16 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    store: new CookieStore({ mongooseConnection: mongoose.connection })
+})
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter); 
